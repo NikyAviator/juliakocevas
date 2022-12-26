0524b07a1b2b7e5b53c49f6618e023d91bc2731a
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-12-26 15:55:46 +0100

    base project setup.

---
+++ juliakocevas/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Importing Pages components
+
+// import Sass
+import './scss/main.scss';
+
+export default function App() {
+  return (
+    <>
+      <div>HELLO</div>
+    </>
+  );
+}
