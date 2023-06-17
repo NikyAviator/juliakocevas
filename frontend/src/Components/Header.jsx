a37c7e6ca9d8fef7bfa5b507dd81feea539da0ca
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-06-17 11:01:02 +0200

    Added some basic navbar

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Header = () => {
-  return <div>Header TEST- TEST</div>;
-};
+function Header() {
+  return (
+    <Navbar bg='light' expand='lg'>
+      <Container>
+        <Navbar.Brand href='#home'>Julia Kocevas</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#mywork'>My Work</Nav.Link>
+            <Nav.Link href='#mywork'>Contanct Me</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
 export default Header;
