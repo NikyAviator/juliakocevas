ef07223262d479e6e5bd5ae02cf90f63af664257
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-06-24 17:34:25 +0200

    centered footer buttons

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaFacebook, FaInstagram, FaTiktok } from 'react-icons/fa';
 
+/* 
+added target='_blank' to open the link in a new tab, and rel='noopener noreferrer' for security reasons. 
+The className='text-white mx-2' is used to make the icons white and add some horizontal margin around them.
+
+*/
+
 const Footer = () => {
   return (
     <footer className='bg-secondary text-white'>
-      <div className='container'>
+      <div className='container py-1 d-flex justify-content-center'>
         <div>
           <a
             href='https://www.facebook.com'
