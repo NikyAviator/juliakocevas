c0660516c8f09aa6d052d56a5335616dd424fb9e
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-12-11 14:16:37 +0100

    added some placeholder pics for the cards

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../scss/main.scss';
+import CardJulia from '../Components/CardJulia';
 
 const Home = () => {
   return (
     <>
       <p>Home TEST</p>
-      <div class='container'>bs5-grid-row</div>
-      <a name='' id='' class='btn btn-primary' href='#' role='button'>
-        Button
-      </a>
+      <CardJulia />
     </>
   );
 };
