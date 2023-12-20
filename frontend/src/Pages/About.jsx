9f3c146a80cec997ab64f5ffb18dc9d20dcc8f60
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-12-20 15:31:09 +0100

    Added some initial structure to About page.

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
-
+import BasicCarousel from '../Components/BasicCarousel';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import '../scss/main.scss';
+import happyDog from '../public/happy-dog.png';
 const About = () => {
-  return <div>AboutMePage, poggers in da chatta!!!</div>;
+  return (
+    <Container>
+      <Row>
+        <Col md={6} className='d-flex align-items-center'>
+          <div>
+            <h2>About Julia</h2>
+            <p>Here's a paragraph about Julia...</p>
+            {/* Add more content as needed */}
+          </div>
+        </Col>
+        <Col
+          md={6}
+          className='d-flex justify-content-center align-items-center'
+        >
+          <Image src={happyDog} alt='Julia' fluid />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <BasicCarousel />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default About;
