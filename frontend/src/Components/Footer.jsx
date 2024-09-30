21b66b6b5fb2359854d79f0f58410f9be41e9559
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-09-30 21:24:39 +0200

    making the footer and navbar gray

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -9,14 +9,14 @@ The className='text-white mx-2' is used to make the icons white and add some hor
 
 const Footer = () => {
   return (
-    <footer className='bg-secondary text-dark'>
+    <footer className='custom-footer-bgcolor'>
       <div className='container py-1 d-flex justify-content-center'>
         <div>
           <a
             href='https://www.instagram.com/juliakocevas'
             target='_blank'
             rel='noopener noreferrer'
-            className='text-white mx-2'
+            className='dark-icon mx-2'
           >
             <FaInstagram size='2em' />
           </a>
@@ -24,7 +24,7 @@ const Footer = () => {
             href='https://www.tiktok.com/juliakocevas'
             target='_blank'
             rel='noopener noreferrer'
-            className='text-white mx-2'
+            className='dark-icon mx-2'
           >
             <FaTiktok size='2em' />
           </a>
