f562257f0589f4b6ba7bda1f54c82bcf97e28de3
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-10-09 18:34:43 +0200

    Updated AG

--- frontend/src/Components/ArtGallery.jsx
+++ frontend/src/Components/ArtGallery.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { mediaFiles } from '../../public';
+
 import { useState, useEffect } from 'react';
 // import InfiniteScroll from 'react-infinite-scroll-component';
 const ArtGallery = () => {
@@ -17,24 +17,31 @@ const ArtGallery = () => {
   }, []);
 
   return (
-    <div className='art-gallery'>
-      {mediaFiles.map((file, index) => {
-        const isImage = file.endsWith('.jpg') || file.endsWith('.png');
-        const isVideo = file.endsWith('.mp4');
-        return (
-          <div key={index} className='media-item'>
-            {isImage && <img src={`/${file}`} alt={file} />}
-            {isVideo && (
-              <video width='320' height='240' controls>
-                <source src={`/${file}`} type='video/mp4' />
-                Your browser does not support the video tag.
-              </video>
-            )}
-            <p>{file}</p>
-          </div>
-        );
-      })}
-    </div>
+    <Container className='art-gallery'>
+      <Row>
+        {mediaFiles.map((file, index) => {
+          const isImage = file.endsWith('.jpg') || file.endsWith('.png');
+          const isVideo = file.endsWith('.mp4');
+
+          return (
+            <Col key={index} md={4} className='mb-4'>
+              <div className='media-item'>
+                {isImage && (
+                  <img src={`/${file}`} alt={file} className='img-fluid' />
+                )}
+                {isVideo && (
+                  <video width='100%' height='auto' controls>
+                    <source src={`/${file}`} type='video/mp4' />
+                    Your browser does not support the video tag.
+                  </video>
+                )}
+                <p>{file}</p>
+              </div>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
   );
 };
 
