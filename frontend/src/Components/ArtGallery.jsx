91091872cadb2ac5f4bff5178cf4c7c77a3c1077
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-10-09 16:23:58 +0200

    Pics added

--- frontend/src/Components/ArtGallery.jsx
+++ frontend/src/Components/ArtGallery.jsx
@@ -1,47 +1,34 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ImageAndTextCard from './ImageAndTextCard';
+import { mediaFiles } from '../../public';
 import { useState, useEffect } from 'react';
 // import InfiniteScroll from 'react-infinite-scroll-component';
 
 const ArtGallery = () => {
-  const [artworks, setArtworks] = useState([]);
-  const [page, setPage] = useState(1);
-
-  // Fetch data from the API
-  useEffect(() => {
-    fetchArtworks(page);
-  }, [page]);
-
-  const fetchArtworks = async (page) => {
-    try {
-      const response = await fetch(
-        `https://picsum.photos/v2/list?page=${page}&limit=6`
-      );
-      const data = await response.json();
-
-      const newArtworks = data.map((item) => ({
-        imageUrl: item.download_url,
-        text: item.author,
-      }));
-
-      setArtworks([...artworks, ...newArtworks]);
-
-      setPage(page + 1);
-    } catch (error) {
-      console.error('Error fetching data: ', error);
-    }
-  };
-
   return (
     <Container>
       <Row>
-        {artworks.map((art, index) => (
-          <Col key={index} md={4}>
-            <ImageAndTextCard imageUrl={art.imageUrl} text={art.text} />
-          </Col>
-        ))}
+        <Col>
+          <div className='art-gallery'>
+            {mediaFiles.map((file, index) => {
+              const isImage = file.endsWith('.jpg') || file.endsWith('.png');
+              const isVideo = file.endsWith('.mp4');
+              return (
+                <div key={index} className='media-item'>
+                  {isImage && <img src={`/${file}`} alt={file} />}
+                  {isVideo && (
+                    <video width='320' height='240' controls>
+                      <source src={`/${file}`} type='video/mp4' />
+                      Your browser does not support the video tag.
+                    </video>
+                  )}
+                  <p>{file}</p>
+                </div>
+              );
+            })}
+          </div>{' '}
+        </Col>
       </Row>
     </Container>
   );
