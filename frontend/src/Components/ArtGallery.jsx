8434037dc8a3d520ea51c6c84733cfb6a8b5c507
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-10-11 11:04:27 +0200

    Starting Image.jsx extraction

--- frontend/src/Components/ArtGallery.jsx
+++ frontend/src/Components/ArtGallery.jsx
@@ -1,10 +1,10 @@
+import '../scss/styles.scss';
 import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-import '../scss/styles.scss';
 
 const ArtGallery = () => {
   const [mediaFiles, setMediaFiles] = useState([]);
---
+++ frontend/src/Components/Image.jsx
@@ -0,0 +1,26 @@
+import '../scss/styles.scss';
+import Col from 'react-bootstrap/esm/Col';
+const Image = (props) => {
+  const { file, index, handleClick } = props;
+  const isImage = file.endsWith('.jpg') || file.endsWith('.png');
+  const isVideo = file.endsWith('.mp4');
+
+  return (
+    <Col key={index} md={4} className='mb-4'>
+      <div className='media-item' onClick={() => handleClick(file)}>
+        {isImage && (
+          <img src={file} alt={`Artwork ${index}`} className='img-fluid' />
+        )}
+        {isVideo && (
+          <video width='100%' height='auto' controls>
+            <source src={file} type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
+        )}
+        <p>{file}</p>
+      </div>
+    </Col>
+  );
+};
+
+export default Image;
