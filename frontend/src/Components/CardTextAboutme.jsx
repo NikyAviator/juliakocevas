61e3dfe9fc07459b05bfe56bf2cf3f02be15fdce
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-10-12 19:00:12 +0200

    Scss improved in Aboutme

--- frontend/src/Components/CardTextAboutme.jsx
+++ frontend/src/Components/CardTextAboutme.jsx
@@ -1,6 +1,8 @@
+import '../scss/styles.scss';
+
 const CardTextAboutme = () => {
   return (
-    <div>
+    <div className='text-wrapper'>
       {' '}
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia
       odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
