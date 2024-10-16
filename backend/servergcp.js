9b044ac8689302bd4c7ecf1c61c8d31654047085
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-10-16 17:41:29 +0200

    Updated servergcp.js CORS 8080

--- backend/servergcp.js
+++ backend/servergcp.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 // Enable CORS for requests from http://localhost:5173
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:8080',
   })
 );
 
