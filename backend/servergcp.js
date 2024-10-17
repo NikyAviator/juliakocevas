13338ada735651eda17d27290d206f2f0595b971
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-10-17 14:04:35 +0200

    npm u all

--- backend/servergcp.js
+++ backend/servergcp.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 
-// Enable CORS for requests from http://localhost:5173
+// Enable CORS for requests from the frontend
 app.use(
   cors({
     origin: 'http://localhost:8080',
